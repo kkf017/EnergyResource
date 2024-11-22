4c12d83326ee2f93ce048d87b7bd6b31eb1f1ec1
kkf017 <cpunk017@gmail.com>
2024-11-22 16:56:27 +0100

    Create EnergyRessourceApp

---
+++ EnergyRessourceApp/services/EnergyResource/management/commands/clear_database.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+
+from EnergyResource.models import *
+
+
+
+class Command(BaseCommand):
+    """
+        Class to implement the clear_database command.
+        args:
+            python3 manage.py clear_database
+    """
+    def handle(self, *args, **options):
+        for x in getEnergyResourcesModel()["ressources"]:
+            removeEnergyResourceModel(x["eid"])
+ 
+ 
